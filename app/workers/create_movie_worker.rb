cdbd7d3489e50c38b33ab483de04b7a619def342
Karolina Sularz <karolina.sularz@smartpension.co.uk>
2021-06-15 13:52:19 +0200

    Solve the first task

---
+++ app/workers/create_movie_worker.rb
@@ -0,0 +1,10 @@
+class CreateMovieWorker
+
+  include Sidekiq::Worker
+  sidekiq_options queue: :movies, retry: false
+
+  def perform(*args)
+    Movie.create(title: Faker::Movie.title)
+  end
+
+end
